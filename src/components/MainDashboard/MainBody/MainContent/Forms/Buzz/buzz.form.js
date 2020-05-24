439e41ff45a3a6b602bd5a46fca3daebcfed6870
saksham <saksham.sachdeva@tothenew.com>
2020-05-24 22:53:50 +0530

    css added

--- src/components/MainDashboard/MainBody/MainContent/Forms/Buzz/buzz.form.js
+++ src/components/MainDashboard/MainBody/MainContent/Forms/Buzz/buzz.form.js
@@ -4,12 +4,16 @@ import * as BuzzState from "./BuzzState.util";
 import classes from "./buzz.module.css";
 import Input from "../../../../UI/Input/input";
 import Wrapper from "../../../../UI/Wrapper/Wrapper";
+import ImgUpld from "../../../../UI/imageIcon/Imageicon";
+import axios from "axios";
 
 const ActivityForm = (props) => {
   const [BuzzForm, setBuzzForm] = useState(BuzzState.Activity_Buzz);
+  const [attachmentPath, setAttachmentPath] = useState(null);
   const [categoryValue, setCategoryValue] = useState(
     BuzzState.Buzz_category.value
   );
+
   const inputChangeHandler = (event, inputIdentifier) => {
     const currstate = { ...BuzzForm };
     const changeInput = { ...currstate[inputIdentifier] };
@@ -26,6 +30,41 @@ const ActivityForm = (props) => {
       return;
     }
     setBuzzForm(BuzzState.Valuable_Buzz);
+  };
+
+  const imageattachmentHandler = (event) => {
+    console.log(event.target.files[0]);
+
+    setAttachmentPath(event.target.files[0]);
+  };
+
+  const SubmitHandler = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+
+    Object.keys(BuzzForm).forEach((keys) => {
+       formData.append(keys , BuzzForm[keys].value)
+    });
+    formData.append('img' , attachmentPath)
+
+    
+    console.log('this is uploaded image')
+    console.log(FormData.img)
+    formData.append("email" , "saksham.sachdeva@tothenew.com");
+
+    if (categoryValue === "Activity") {
+      console.log("activity buzz created");
+    
+      axios
+        .post("http://localhost:5000/activities", formData )
+        .then((result) => console.log(result));
+    } else if (categoryValue === "Lost&Found") {
+    
+  
+    } else {
+      alert("please select category to submit");
+    }
   };
 
   const formBody = [];
@@ -60,8 +99,13 @@ const ActivityForm = (props) => {
         <form className={classes.buzzForm}>
           <div className={classes.inputFields}>{formBody}</div>
           <div className={classes.categoryDiv}>
-            {categoryField}
-            <i class="fas fa-location-arrow"></i>
+            <div className={classes.categoryFieldsDiv}>
+              {categoryField}
+              <ImgUpld fileuploadDisplayHandler={imageattachmentHandler} />
+            </div>
+            <button onClick={SubmitHandler} className={classes.submitbtn}>
+              <i class="fas fa-location-arrow"></i>
+            </button>
           </div>
         </form>
       </Wrapper>
